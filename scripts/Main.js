730c6fcd98c66ece3ddb5240aab4a6af1d16f87e
JulianoKosloski <juliano.koslos@gmail.com>
2022-09-21 11:04:59 -0300

    dark mode switch working babyyy

--- scripts/Main.js
+++ scripts/Main.js
@@ -3,14 +3,24 @@ const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        image = document.getElementsById('sic-logo');
-        image.src = "images/sic2.png";
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-        image = document.getElementsById('sic-logo');
-        image.src = "images/sic1.png";
     }    
 }
 
+// function switchLogo(e) {
+//     if (e.target.checked) {
+//         document.documentElement.setAttribute('data-theme', 'dark');
+//         var image = document.getElementsById('sic-logo');
+//         image.src = "images/sic2.png";
+//     }
+//     else {
+//         document.documentElement.setAttribute('data-theme', 'light');
+//         var image = document.getElementsById('sic-logo');
+//         image.src = "images/sic1.png";
+//     }    
+// }
+
 toggleSwitch.addEventListener('change', switchTheme, false);
+// toggleSwitch.addEventListener('change', switchLogo, false);
